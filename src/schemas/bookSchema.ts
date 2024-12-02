32731ee68b1d4d0572b82e208bdb344be1356da1
thais ventura <thaisvtr26@gmail.com>
2024-12-01 23:24:14 -0300

    :art: refactor: enhance bookSchema with required fields and improved validation messages

--- src/schemas/bookSchema.ts
+++ src/schemas/bookSchema.ts
@@ -1,11 +1,15 @@
 import * as z from "zod";
+
 export const bookSchema = z.object({
-  title: z.string(),
-  author: z.string(),
-  description: z.string(),
-  pages: z.coerce.number().positive(),
-  image: z.string(),
-  authorImage: z.string(),
+  title: z.string().nonempty("O título é obrigatório."),
+  author: z.string().nonempty("O autor é obrigatório."),
+  description: z.string().nonempty("A descrição é obrigatória."),
+  pages: z.coerce
+    .number()
+    .positive("O número de páginas deve ser maior que zero.")
+    .int("O número de páginas deve ser um número inteiro."),
+  image: z.string().optional(),
+  authorImage: z.string().optional(),
 });
 
 export type bookFormType = z.infer<typeof bookSchema>;
