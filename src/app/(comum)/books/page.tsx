16282b97dbcd3596390d97f56b919deecadd442b
thais ventura <thaisvtr26@gmail.com>
2024-12-02 02:01:40 -0300

    :art: refactor: improve layout and responsiveness of the Books page

--- src/app/(comum)/books/page.tsx
+++ src/app/(comum)/books/page.tsx
@@ -145,17 +145,17 @@ export default function Books() {
     }
   };
   return (
-    <main className="flex h-svh pr-10">
+    <main className="flex flex-wrap md:flex-nowrap h-svh p-4 md:p-10">
       <Sidebar />
       <div className="flex flex-col w-full pb-10 mt-4 gap-10">
         <div className="flex flex-col gap-3">
-          <div className="flex flex-row w-full justify-between">
-            <div className="flex flex-col w-[500px]">
-              <h1 className="text-start text-white font-montserrat font-normal text-2xl">
+          <div className="flex flex-col md:flex-row w-full justify-between">
+            <div className="flex flex-col w-full md:w-[500px]">
+              <h1 className="text-start text-white font-montserrat font-normal text-xl md:text-2xl">
                 Livros adicionados
               </h1>
               <div className="flex flex-col gap-3">
-                <p className="font-montserrat text-base font-normal text-zinc-600">
+                <p className="font-montserrat text-sm md:text-base font-normal text-zinc-600">
                   Adicione um novo livro à sua coleção.
                 </p>
                 <Button
@@ -166,11 +166,11 @@ export default function Books() {
                 </Button>
               </div>
             </div>
-            <div className="flex flex-col items-start gap-4 w-[500px] px-5">
+            <div className="flex flex-col items-start gap-4 w-full md:w-[500px] px-5">
               {books && (
                 <>
                   <div className="flex flex-row justify-start items-center gap-2">
-                    <Avatar className="w-20 h-20">
+                    <Avatar className="w-16 h-16 md:w-20 md:h-20">
                       <AvatarImage
                         className="w-full"
                         src={
@@ -181,7 +181,7 @@ export default function Books() {
                       />
                     </Avatar>
                     <div className="flex flex-col">
-                      <h3 className="text-white font-semibold font-montserrat">
+                      <h3 className="text-white font-semibold font-montserrat text-sm md:text-base">
                         {books[selectedBookIndex]?.author ||
                           "Autor desconhecido"}
                       </h3>
@@ -190,7 +190,7 @@ export default function Books() {
                       </p>
                     </div>
                   </div>
-                  <p className="italic font-light font-montserrat text-xs h-[80px] text-zinc-600">
+                  <p className="italic font-light font-montserrat text-xs md:text-sm h-[80px] text-zinc-600">
                     {books[selectedBookIndex]?.description ||
                       "Citação não disponível para este autor."}
                   </p>
@@ -239,15 +239,15 @@ export default function Books() {
             <p className="text-zinc-600">Erro ao carregar livros.</p>
           ) : (
             <>
-              <div className="flex justify-start px-5 gap-10">
+              <div className="flex flex-wrap justify-start px-5 gap-4 md:gap-10">
                 {filteredBooks &&
                   filteredBooks.map((book, index) => (
                     <div
                       key={book.id}
                       className={`relative group flex flex-col items-center transition-all duration-300 ${
                         index === selectedBookIndex
-                          ? "w-[210px] h-[350px]"
-                          : "w-[200px] h-[300px]"
+                          ? "w-[160px] h-[280px] md:w-[210px] md:h-[350px]"
+                          : "w-[140px] h-[240px] md:w-[200px] md:h-[300px]"
                       }`}
                     >
                       <div className="relative w-full h-full">
@@ -275,13 +275,16 @@ export default function Books() {
                                 <Trash2 />
                               </button>
                             </AlertDialogTrigger>
-                            <AlertDialogContent>
+                            <AlertDialogContent className="bg-black">
                               <AlertDialogHeader>
-                                <AlertDialogTitle>
+                                <AlertDialogTitle className="text-white font-montserrat">
                                   Deletar Livro
                                 </AlertDialogTitle>
-                                <AlertDialogDescription>
+                                <AlertDialogDescription className="font-montserrat text-sm">
                                   Tem certeza que deseja deletar o livro{" "}
+                                  <span className="font-montserrat text-zinc-800">
+                                    {" "}
+                                  </span>
                                   <strong>{bookToDelete?.title}</strong>? Essa
                                   ação não pode ser desfeita.
                                 </AlertDialogDescription>
@@ -307,104 +310,105 @@ export default function Books() {
         </div>
       </div>
       {isAddModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-zinc-900 bg-opacity-60 z-50">
-          <Card className="relative w-fit bg-black shadow-xl p-8 rounded-lg text-white">
-            <button
-              onClick={handleCloseAddModal}
-              className="absolute top-4 right-4 text-gray-400 hover:text-gray-200 focus:outline-none"
-              aria-label="Fechar"
-            >
-              <X size={20} />
-            </button>
-            <CardHeader>
-              <CardTitle className="text-xl font-bold font-montserrat">
-                Adicionar Novo Livro
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <form
-                onSubmit={handleSubmit(onAddSubmit)}
-                className="flex flex-col gap-4"
-              >
-                <div className="grid grid-cols-2 gap-4">
-                  <Input
-                    label="Título"
-                    {...register("title")}
-                    placeholder="Digite o título"
-                  />
-                  <Input
-                    label="Autor"
-                    {...register("author")}
-                    placeholder="Nome do autor"
-                  />
-                </div>
-                {errors.title && (
-                  <p className="text-red-500 text-xs">{errors.title.message}</p>
-                )}
-                {errors.author && (
-                  <p className="text-red-500 text-xs">
-                    {errors.author.message}
-                  </p>
-                )}
+  <div className="fixed inset-0 flex items-center justify-center bg-zinc-900 bg-opacity-60 z-50">
+    <Card className="relative w-full max-w-md bg-black shadow-xl p-8 rounded-lg text-white">
+      <button
+        onClick={handleCloseAddModal}
+        className="absolute top-4 right-4 text-gray-400 hover:text-gray-200 focus:outline-none"
+        aria-label="Fechar"
+      >
+        <X size={20} />
+      </button>
+      <CardHeader>
+        <CardTitle className="text-xl font-bold font-montserrat">
+          Adicionar Novo Livro
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form
+          onSubmit={handleSubmit(onAddSubmit)}
+          className="flex flex-col gap-4"
+        >
+          <div className="grid grid-cols-2 sm:grid-cols-1 gap-4">
+            <Input
+              label="Título"
+              {...register("title")}
+              placeholder="Digite o título"
+            />
+            <Input
+              label="Autor"
+              {...register("author")}
+              placeholder="Nome do autor"
+            />
+          </div>
+          {errors.title && (
+            <p className="text-red-500 text-xs">{errors.title.message}</p>
+          )}
+          {errors.author && (
+            <p className="text-red-500 text-xs">
+              {errors.author.message}
+            </p>
+          )}
 
-                <div className="grid grid-cols-2 gap-4">
-                  <Input
-                    label="Páginas"
-                    type="number"
-                    {...register("pages")}
-                    placeholder="Número de páginas"
-                  />
-                  <Input
-                    label="Capa do Autor"
-                    {...register("authorImage")}
-                    placeholder="URL da imagem do autor"
-                  />
-                </div>
-                {errors.pages && (
-                  <p className="text-red-500 text-xs">{errors.pages.message}</p>
-                )}
-                {errors.authorImage && (
-                  <p className="text-red-500 text-xs">
-                    {errors.authorImage.message}
-                  </p>
-                )}
+          <div className="grid grid-cols-2 sm:grid-cols-1 gap-4">
+            <Input
+              label="Páginas"
+              type="number"
+              {...register("pages")}
+              placeholder="Número de páginas"
+            />
+            <Input
+              label="Capa do Autor"
+              {...register("authorImage")}
+              placeholder="URL da imagem do autor"
+            />
+          </div>
+          {errors.pages && (
+            <p className="text-red-500 text-xs">{errors.pages.message}</p>
+          )}
+          {errors.authorImage && (
+            <p className="text-red-500 text-xs">
+              {errors.authorImage.message}
+            </p>
+          )}
 
-                <Input
-                  label="Capa do Livro"
-                  {...register("image")}
-                  placeholder="URL da capa do livro"
-                />
-                {errors.image && (
-                  <p className="text-red-500 text-xs">{errors.image.message}</p>
-                )}
+          <Input
+            label="Capa do Livro"
+            {...register("image")}
+            placeholder="URL da capa do livro"
+          />
+          {errors.image && (
+            <p className="text-red-500 text-xs">{errors.image.message}</p>
+          )}
 
-                <Textarea
-                  label="Descrição"
-                  {...register("description")}
-                  placeholder="Digite uma breve descrição"
-                  className="col-span-2"
-                />
-                {errors.description && (
-                  <p className="text-red-500 text-xs">
-                    {errors.description.message}
-                  </p>
-                )}
+          <Textarea
+            label="Descrição"
+            {...register("description")}
+            placeholder="Digite uma breve descrição"
+            className="col-span-2"
+          />
+          {errors.description && (
+            <p className="text-red-500 text-xs">
+              {errors.description.message}
+            </p>
+          )}
 
-                <Button
-                  isLoading={isAdding}
-                  className="w-full bg-amber-500 font-semibold hover:bg-amber-400"
-                >
-                  Adicionar
-                </Button>
-              </form>
-            </CardContent>
-          </Card>
-        </div>
-      )}
+          <Button
+            isLoading={isAdding}
+            className="w-full bg-amber-500 font-semibold hover:bg-amber-400"
+          >
+            Adicionar
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
+  </div>
+)}
+
 
       {isEditModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-zinc-900 bg-opacity-60 z-50">
-          <Card className="relative w-[400px] bg-black shadow-xl p-8 rounded-lg text-white">
+          <Card className="relative w-fit bg-black shadow-xl p-8 rounded-lg text-white">
             <button
               onClick={handleCloseEditModal}
               className="absolute top-4 right-4 text-gray-400 hover:text-gray-200 focus:outline-none"
