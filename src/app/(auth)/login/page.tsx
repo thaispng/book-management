8e1868e30e53f25ce1bc6905c7f2c8fae721b5aa
thais ventura <thaisvtr26@gmail.com>
2024-12-01 23:22:26 -0300

    feat: import Userservice in login page for user authentication

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-
+import Userservice from '@/service/useService';
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
